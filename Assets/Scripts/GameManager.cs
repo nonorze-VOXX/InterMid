495bdc7fef5bdb40be7c544ac7cf8aad22a4f59f
green-Desktop <appieosugood@gmail.com>
2024-11-05 23:22:56 +0800

    remove game flow file

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -26,7 +26,6 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Player playerPrefab;
 
     [SerializeField] private TMP_Text welcomeText;
-    // IGameFlow _gameFlow;
 
     private readonly HashSet<Player> playerPrepared = new();
     private GameState _gameState;
